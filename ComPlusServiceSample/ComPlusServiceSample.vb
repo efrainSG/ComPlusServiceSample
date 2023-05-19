1135974280eaa189145728698786e0286ec47ca6
Efrain <efrain_serna@hotmail.com>
2023-05-18 19:33:44 -0600

    Agregar archivos de proyecto.

---
+++ ComPlusServiceSample/ComPlusServiceSample.vb
@@ -0,0 +1,19 @@
+﻿Imports System.EnterpriseServices
+Imports System.Reflection
+
+<Assembly: ApplicationName("ComPlusServiceSample")>
+<Assembly: AssemblyKeyFile("./bin/Debug/ComPlusServiceSample.snk")>
+
+<Transaction(TransactionOption.Required)>
+Public Class ComPlusServiceSample
+    Inherits ServicedComponent
+
+    Public Sub New()
+        MyBase.New()
+    End Sub
+
+    <AutoComplete()> Public Function DoTransaction() As String
+        Return "Servicio COM+ Funciona correctamente..."
+    End Function
+
+End Class
